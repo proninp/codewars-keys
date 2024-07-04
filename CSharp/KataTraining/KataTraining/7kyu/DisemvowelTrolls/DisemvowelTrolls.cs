fb6d02c067d42d6945addf99f5c68ec1b5057f08
proninp <proninpaul@yandex.ru>
2024-07-04 15:22:55 +0300

    7kyu Disemvowel Trolls [2024.07.04]

---
+++ CSharp/KataTraining/KataTraining/7kyu/DisemvowelTrolls/DisemvowelTrolls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTraining._7kyu.DisemvowelTrolls
+{
+    public class DisemvowelTrolls
+    {
+        private static List<char> _vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        public static string Disemvowel(string str) => string.Concat(str.Where(c => !_vowels.Contains(c)));
+    }
+}
