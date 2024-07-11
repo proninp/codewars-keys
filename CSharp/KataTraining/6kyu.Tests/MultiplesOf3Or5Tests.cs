3e749692efae86a9e79b5b4f103700cc9f569179
proninp <velninpapro@gmail.com>
2024-07-11 08:08:28 +0300

    6kyu Multiples of 3 or 5 [2024.07.11]

---
+++ CSharp/KataTraining/6kyu.Tests/MultiplesOf3Or5Tests.cs
@@ -0,0 +1,23 @@
+﻿using _6kyu.Multiples_of_3_or_5;
+
+namespace _6kyu.Tests
+{
+    public class MultiplesOf3Or5Tests
+    {
+        [Theory]
+        [InlineData(10, 23)]
+        [InlineData(20, 78)]
+        [InlineData(200, 9168)]
+        [InlineData(0, 0)]
+        public static void SolutionTest(int value, int expected)
+        {
+            // Arrange
+
+            // Act
+            int actual = MultiplesOf3Or5.Solution(value);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
