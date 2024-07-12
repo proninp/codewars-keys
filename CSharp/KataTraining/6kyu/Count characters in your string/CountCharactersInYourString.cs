c1967f1d93b8ced15410d757a12e524768b5cea1
proninp <velninpapro@gmail.com>
2024-07-12 08:21:34 +0300

    6kyu Count characters in your string [2024.07.12]

---
+++ CSharp/KataTraining/6kyu/Count characters in your string/CountCharactersInYourString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6kyu.Count_characters_in_your_string
+{
+    public class CountCharactersInYourString
+    {
+        public static Dictionary<char, int> Count(string str) =>
+            str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+    }
+}
