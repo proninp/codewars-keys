4526117036bc6da9ad969ac32c6dd99b600f27bd
proninp <velninpapro@gmail.com>
2024-07-13 10:20:06 +0300

    7kyu Simple Fun #176: Reverse Letter [2024.07.13]

---
+++ CSharp/KataTraining/7kyu.Tests/ReverseLetterTaskTests.cs
@@ -0,0 +1,28 @@
+﻿using _7kyu.Simple_Fun_176._Reverse_Letter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7kyu.Tests
+{
+    public class ReverseLetterTaskTests
+    {
+        [Theory]
+        [InlineData("krishan", "nahsirk")]
+        [InlineData("ultr53o?n", "nortlu")]
+        [InlineData("ab23c", "cba")]
+        [InlineData("krish21an", "nahsirk")]
+        public static void ReverseLetterTest(string str, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = ReverseLetterTask.ReverseLetter(str);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CSharp/KataTraining/7kyu/Simple Fun 176. Reverse Letter/ReverseLetterTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _7kyu.Simple_Fun_176._Reverse_Letter
+{
+    public class ReverseLetterTask
+    {
+        public static string ReverseLetter(string str)
+        {
+           return new string(str.Reverse().Where(char.IsLetter).ToArray());
+        }
+    }
+}
