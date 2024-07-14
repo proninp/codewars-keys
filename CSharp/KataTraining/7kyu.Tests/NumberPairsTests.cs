56fa75da5b695c13a7c172f37ea0661258a779b9
proninp <velninpapro@gmail.com>
2024-07-14 19:48:31 +0300

    7kyu Number Pairs [2024.07.14]

---
+++ CSharp/KataTraining/7kyu.Tests/NumberPairsTests.cs
@@ -0,0 +1,22 @@
+﻿using _7kyu.Number_Pairs;
+
+namespace _7kyu.Tests
+{
+    public class NumberPairsTests
+    {
+        [Fact]
+        public static void GetLargerNumbersTest()
+        {
+            // Arrange
+            int[] arr1 = new int[] { 13, 64, 5, 7, 88 };
+            int[] arr2 = new int[] { 23, 4, 53, 17, 80 };
+
+            // Act
+            var actual = NumberPairs.GetLargerNumbers(arr1, arr2);
+            var expected = new int[] { 23, 64, 53, 17, 88 };
+
+            // Assert
+            Assert.True(Enumerable.SequenceEqual(expected, actual));
+        }
+    }
+}
---
+++ CSharp/KataTraining/7kyu/Number Pairs/NumberPairs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _7kyu.Number_Pairs
+{
+    public class NumberPairs
+    {
+        public static int[] GetLargerNumbers(int[] a, int[] b)
+        {
+            return a.Select((x, i) => Math.Max(x, b[i])).ToArray();
+        }
+    }
+}
