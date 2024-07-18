2d128500a11b277e0864f7d27bb000bea2dc8c68
proninp <velninpapro@gmail.com>
2024-07-18 16:48:48 +0300

    7kyu Simple beads count [2024.07.18]

---
+++ CSharp/KataTraining/7kyu/Simple beads count/SimpleBeadsCount.cs
@@ -0,0 +1,10 @@
+﻿namespace _7kyu.Simple_beads_count
+{
+    public class SimpleBeadsCount
+    {
+        public static int CountRedBeads(int n)
+        {
+            return n < 2 ? 0 : (n - 1) * 2;
+        }
+    }
+}
