a5507fb2f78557e94717fbc9b8f48b1446fcb174
proninp <velninpapro@gmail.com>
2024-07-19 16:21:26 +0300

    7kyu RGB To Hex Conversion [2024.07.19]

---
+++ CSharp/KataTraining/5kyu.Tests/RGBToHexConversionTests.cs
@@ -0,0 +1,27 @@
+﻿using _5kyu.RGB_To_Hex_Conversion;
+
+namespace _5kyu.Tests
+{
+    public class RGBToHexConversionTests
+    {
+        [Theory]
+        [InlineData(255, 255, 255, "FFFFFF")]
+        [InlineData(255, 255, 300, "FFFFFF")]
+        [InlineData(0, 0, 0, "000000")]
+        [InlineData(148, 0, 211, "9400D3")]
+        [InlineData(148, -20, 211, "9400D3")]
+        [InlineData(144, 195, 212, "90C3D4")]
+        [InlineData(212, 53, 12, "D4350C")]
+        public static void RgbTest(int r, int g, int b, string expected)
+        {
+            // Arrange
+
+
+            // Act
+            string actual = RGBToHexConversion.Rgb(r, g, b);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- CSharp/KataTraining/5kyu/Program.cs
+++ CSharp/KataTraining/5kyu/Program.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using _5kyu.RGB_To_Hex_Conversion;
+using System;
 namespace _5kyu
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
