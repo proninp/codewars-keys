d9603d3974ebf9c9a3b92cc3eba3bc91cb138fba
proninp <velninpapro@gmail.com>
2024-07-27 14:39:38 +0300

    7kyu - Disemvowel Trolls - [2024.07.27]

---
+++ CSharp/Kata/Kata/7-kyu/Disemvowel_Trolls/src/DisemvowelTrolls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata._7_kyu.Disemvowel_Trolls.src
+{
+    public class DisemvowelTrolls
+    {
+        private static List<char> _vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        public static string Disemvowel(string str) => string.Concat(str.Where(c => !_vowels.Contains(c)));
+    }
+}
---
+++ CSharp/Kata/Kata/7-kyu/Disemvowel_Trolls/test/DisemvowelTrollsTest.cs
@@ -0,0 +1,43 @@
+﻿using Kata._7_kyu.Disemvowel_Trolls.src;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata._7_kyu.Disemvowel_Trolls.test
+{
+    [TestFixture]
+    public class DisemvowelTrollsTest
+    {
+        [TestCase("This website is for losers LOL!", "Ths wbst s fr lsrs LL!")]
+        [TestCase("No offense but,\nYour writing is among the worst I've ever read", "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")]
+        [TestCase("What are you, a communist?", "Wht r y,  cmmnst?")]
+        public void FixedTests(string str, string expected)
+        {
+            ClassicAssert.AreEqual(expected, DisemvowelTrolls.Disemvowel(str));
+        }
+
+        [TestCaseSource(nameof(GenerateTestCases))]
+        public string RandomTests(string str) => DisemvowelTrolls.Disemvowel(str);
+
+        private static IEnumerable<TestCaseData> GenerateTestCases()
+        {
+            string ReferenceSolution(string refStr)
+            {
+                bool IsVowel(char c) => new[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' }.Contains(c);
+                return new string(refStr.Where(c => !IsVowel(c)).ToArray());
+            }
+            var rnd = new Random();
+            int RandomInt(int a, int b) => (int)(rnd.Next(b - a)) + a;
+            var chars = "abcdefghijklmnopqrsruvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!,@#$%^&*()[]{};         ";
+            char randomChar() => chars[RandomInt(0, chars.Length)];
+            for (var i = 0; i < 100; ++i)
+            {
+                var str = new string(Enumerable.Range(0, RandomInt(20, 100)).Select(_ => randomChar()).ToArray());
+                var expected = ReferenceSolution(str);
+                yield return new TestCaseData(str).Returns(expected).SetDescription($"str = \"{str}\"");
+            }
+        }
+    }
+}
