7505a4fa4d939e9a469e73bcb95ef3f16f515bcb
proninp <velninpapro@gmail.com>
2024-09-02 16:02:26 +0300

    Added kata's readme - [2024.09.02]

--- CSharp/Kata/Kata/Program.cs
+++ CSharp/Kata/Kata/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Kata
+﻿using System.IO;
+using KataReadmeBuilder;
+
+namespace Kata;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            
-        }
+        ReadmeBuilder.BuildReadmeFile(Directory.GetCurrentDirectory());
     }
 }
