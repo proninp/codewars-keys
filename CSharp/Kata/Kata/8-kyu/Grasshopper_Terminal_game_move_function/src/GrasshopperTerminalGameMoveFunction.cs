f83d4da05ade0fd25d74825e4579d837097c77f6
proninp <velninpapro@gmail.com>
2024-09-08 17:06:11 +0300

    8kyu - Grasshopper - Terminal game move function - [2024.09.08]

---
+++ CSharp/Kata/Kata/8-kyu/Grasshopper_Terminal_game_move_function/src/GrasshopperTerminalGameMoveFunction.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kata._8_kyu.Grasshopper_Terminal_game_move_function.src;
+public class GrasshopperTerminalGameMoveFunction
+{
+    public static int Move(int position, int roll) =>
+        position + roll + roll;
+}
---
+++ CSharp/Kata/Kata/8-kyu/Grasshopper_Terminal_game_move_function/test/GrasshopperTerminalGameMoveFunctionTest.cs
@@ -0,0 +1,14 @@
+﻿using Kata._8_kyu.Grasshopper_Terminal_game_move_function.src;
+using NUnit.Framework;
+
+namespace Kata._8_kyu.Grasshopper_Terminal_game_move_function.test;
+
+[TestFixture]
+public class GrasshopperTerminalGameMoveFunctionTest
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.That(GrasshopperTerminalGameMoveFunction.Move(0, 4), Is.EqualTo(8));
+    }
+}
