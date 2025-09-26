104bd57a196eae522a36035460a0f61f37108005
proninp <velninpapro@gmail.com>
2025-09-26 23:16:10 +0300

    6kyu - Playing with passphrases

---
+++ Kata/Kata/6-kyu/Playing_with_passphrases/src/PlayingWithPassphrases.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kata._6_kyu.Playing_with_passphrases.src;
+
+public class PlayingWithPassphrases
+{
+    public static string PlayPass(string s, int n)
+    {
+        var charArr = new char[s.Length];
+        for (var i = 0; i < s.Length; i++)
+        {
+            char newChar;
+            if (char.IsDigit(s[i]))
+            {
+                newChar = (char)(105 - s[i]);
+            }
+            else if (char.IsLetter(s[i]))
+            {
+                newChar = (char)((char.ToLower(s[i]) - 'a' + n) % 26 + 'a');
+                newChar = i % 2 == 0 ? char.ToUpper(newChar) : newChar;
+            }
+            else
+            {
+                newChar = s[i];
+            }
+            charArr[s.Length - i - 1] = newChar;
+        }
+        return new string(charArr);
+    }
+}
---
+++ Kata/Kata/6-kyu/Playing_with_passphrases/test/PlayingWithPassphrasesTests.cs
@@ -0,0 +1,23 @@
+﻿using Kata._6_kyu.Playing_with_passphrases.src;
+using NUnit.Framework;
+
+namespace Kata._6_kyu.Playing_with_passphrases.test;
+
+[TestFixture]
+public class PlayingWithPassphrasesTests
+{
+    [Test]
+    public static void Test1()
+    {
+        Assert.That(PlayingWithPassphrases.PlayPass("I LOVE YOU!!!", 1), Is.EqualTo("!!!vPz fWpM J"));
+    }
+
+    [Test]
+    public static void Test2()
+    {
+        Assert.That(PlayingWithPassphrases.PlayPass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2), 
+            Is.EqualTo("4897 NkTrC Hq fT67 GjV Pq aP OqTh gOcE CoPcTi aO"));
+    }
+    
+    
+}
