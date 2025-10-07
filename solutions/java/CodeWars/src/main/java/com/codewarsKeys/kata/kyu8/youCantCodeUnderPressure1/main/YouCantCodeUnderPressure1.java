a83719473f5168457bc5ae092a775860364e9db6
proninp <velninpapro@gmail.com>
2025-10-07 16:45:37 +0300

    8kyu - ou Can't Code Under Pressure #1

---
+++ solutions/java/CodeWars/src/main/java/com/codewarsKeys/kata/kyu8/youCantCodeUnderPressure1/main/YouCantCodeUnderPressure1.java
@@ -0,0 +1,7 @@
+package com.codewarsKeys.kata.kyu8.youCantCodeUnderPressure1.main;
+
+public class YouCantCodeUnderPressure1 {
+    public static int doubleInteger(int i) {
+        return i << 1;
+    }
+}
---
+++ solutions/java/CodeWars/src/main/java/com/codewarsKeys/kata/kyu8/youCantCodeUnderPressure1/test/YouCantCodeUnderPressure1Test.java
@@ -0,0 +1,23 @@
+package com.codewarsKeys.kata.kyu8.youCantCodeUnderPressure1.test;
+
+import com.codewarsKeys.kata.kyu8.youCantCodeUnderPressure1.main.YouCantCodeUnderPressure1;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class YouCantCodeUnderPressure1Test {
+    @Test
+    public final void test_two() {
+        Assert.assertEquals(
+                "Nope!" ,
+                4,
+                YouCantCodeUnderPressure1.doubleInteger(2));
+    }
+
+    @Test
+    public final void test_eight() {
+        Assert.assertEquals(
+                "Nope!" ,
+                16,
+                YouCantCodeUnderPressure1.doubleInteger(8));
+    }
+}
